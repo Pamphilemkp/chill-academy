033e0c37b9d43ec28245b11381628f0a40a4e3e4
pamphilemkp <pamphilemkp@gmail.com>
2023-11-30 10:35:45 +0200

    fix bg body

--- components/Section.js
+++ components/Section.js
@@ -8,24 +8,29 @@ const Section = ()=> {
                     <div className="container mx-auto space-y-16">
                         <section>
                             <span className="block mb-2 text-xs font-medium tracki uppercase lg:text-center dark:text-violet-400">How it works</span>
-                            <h2 className="text-5xl font-bold lg:text-center dark:text-gray-50">Building with Mamba is simple</h2>
+                            <h2 className="text-5xl font-bold lg:text-center dark:text-gray-50"> Chill Academy formula: </h2>
                             <div className="grid gap-6 my-16 lg:grid-cols-3">
                                 <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-900">
                                     <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-400 dark:text-gray-900">1</div>
                                     <p className="text-2xl font-semibold">
-                                        <b>Nulla.</b>Nostrum, corrupti blanditiis. Illum, architecto?
+                                        <b>Listening: <br/> 
+                                        </b>Our formula is simple – we listen, we solve. Your project becomes our priority, and we commit to delivering the best solution possible.
+                                            We actively listen to your ideas and requirements to fully grasp your vision.
+
                                     </p>
                                 </div>
                                 <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-900">
                                     <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-400 dark:text-gray-900">2</div>
                                     <p className="text-2xl font-semibold">
-                                        <b>Accusantium.</b>Vitae saepe atque neque sunt eius dolor veniam alias debitis?
+                                        <b>Problem-Solving:</b> <br/>
+                                        We approach your project as our own, taking ownership of the challenges and finding the best solutions.
                                     </p>
                                 </div>
                                 <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-900">
                                     <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-400 dark:text-gray-900">3</div>
                                     <p className="text-2xl font-semibold">
-                                        <b>Maxime.</b>Expedita temporibus culpa reprehenderit doloribus consectetur odio!
+                                        <b>Reliability:</b> <br/>
+                                        You can count on us to deliver your project on time and within budget.
                                     </p>
                                 </div>
                             </div>
@@ -48,7 +53,11 @@ const Section = ()=> {
                             </div>
                             <div className="p-8 space-y-8 rounded-md lg:col-span-full lg:py-12 dark:bg-gray-900">
                                 <h2 className="text-5xl font-bold dark:text-gray-50">Create with us</h2>
-                                <p className="dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt facilis quod accusantium beatae cum nam adipisci reiciendis omnis possimus error quo animi voluptas magni, at incidunt. Nulla ex at ullam corporis quidem adipisci vitae, perferendis dolorem libero minus atque tenetur enim pariatur cupiditate commodi in beatae, ipsa eligendi? Quis, saepe.</p>
+                                <p className="dark:text-gray-400">
+                                We believe in a collaborative and client-centric approach. We take the time to understand your 
+                                needs and work closely with you throughout the development process. We are committed to delivering
+                                 high-quality solutions that meet your expectations and exceed your goals.
+                                </p>
                             </div>
                         </section>
                         <section>
@@ -57,24 +66,40 @@ const Section = ()=> {
                                     <Image width={1000} height={1000} src="https://source.unsplash.com/random/485x365" alt="" className="object-cover w-full h-auto max-h-96 dark:bg-gray-500" />
                                     <div className="p-6 space-y-6 lg:p-8 md:flex md:flex-col dark:bg-gray-900">
                                         <span className="self-start px-3 py-1 text-sm rounded-full dark:bg-violet-400 dark:text-gray-900">Business</span>
-                                        <h2 className="text-3xl font-bold md:flex-1">Curating a workplace that inspires team movement</h2>
+                                        <h2 className="text-xl md:flex-1">
+                                        {`Already have a project? We're here to improve it. Looking for talented developers? Our pool of skilled professionals is ready
+                                         to showcase their expertise through part-time, full-time, or contract opportunities.`}
+                                        </h2>
                                         <div>
                                             <p className="dark:text-gray-400">November 30, 2020</p>
                                             <p className="dark:text-gray-400">By Leroy Jenkins</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="p-6 rounded lg:p-8 lg:py-12 dark:bg-gray-900">
-							<h3 className="inline font-medium dark:text-gray-50">Panel-based blocks.</h3>
-							<p className="inline">Flexible panels that are perfect for building functional layouts.</p>
+                                <div className="flex flex-col gap-3 justify-center align-center p-6 rounded lg:p-8 lg:py-12 dark:bg-gray-900">
+							<h3 className="font-medium dark:text-gray-50 text-lg">Engaging</h3>
+							<p className="">
+                            {`At Chill Academy, we prioritize active and meaningful engagement with our clients.
+                             We listen attentively, making your ideas our own. This deep involvement ensures
+                              a thorough understanding of your vision and goals, forming the basis for a 
+                              successful collaboration.`}
+                            </p>
 						</div>
-						<div className="p-6 rounded lg:p-8 lg:py-12 dark:bg-gray-900">
-							<h3 className="inline font-medium dark:text-gray-50">Responsive design.</h3>
-							<p className="inline">Panels look great no matter the device.</p>
+						<div className="flex flex-col gap-3 justify-center align-center p-6 rounded lg:p-8 lg:py-12 dark:bg-gray-900">
+							<h3 className="font-medium dark:text-gray-50 text-lg">Creating</h3>
+							<p className="">
+                            {` Once engaged, we take the gathered information and use it to create a comprehensive project plan.
+                             We conceptualize and design solutions tailored to your specific requirements. The emphasis on "creating"
+                              underscores our proactive approach to turning ideas into tangible and effective projects.`}
+                            </p>
 						</div>
-						<div className="p-6 rounded lg:p-8 lg:py-12 dark:bg-gray-900">
-							<h3 className="inline font-medium dark:text-gray-50">Premium support.</h3>
-							<p className="inline">Join over 50 000 satisfied customers who use our templates.</p>
+						<div className="flex flex-col gap-3 justify-center align-center p-6 rounded lg:p-8 lg:py-12 dark:bg-gray-900">
+							<h3 className="font-medium dark:text-gray-50 text-lg">Caring</h3>
+							<p className="">
+                                {`We extend our commitment beyond the planning stage at Chill IT. Caring signifies ongoing support, meticulous
+                                 attention to detail, and a genuine dedication to your project's success. From inception to completion, we ensure
+                                  your satisfaction, reflecting our long-term commitment to the implemented solution's success.`}
+                            </p>
 						</div>
 					</div>
 				</section>
