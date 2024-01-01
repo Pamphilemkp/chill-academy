c0734c753430d03924a72d2622dc00fd6f8f0e45
pamphilemkp <pamphilemkp@gmail.com>
2024-01-01 17:31:26 +0200

    remove unnecessary blocs from section

--- components/Section.js
+++ components/Section.js
@@ -102,53 +102,7 @@ const Section = () => {
               </div>
             </section>
             <section className="grid gap-6 lg:grid-cols-2">
-              <Image
-                width={1000}
-                height={1000}
-                src="https://source.unsplash.com/random/360x480"
-                alt=""
-                className="object-cover w-full rounded-md max-h-96 dark:bg-gray-500"
-              />
-              <div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 bg-gray-400 dark:bg-gray-900">
-                <Image
-                  width={1000}
-                  height={1000}
-                  src="https://source.unsplash.com/random/100x100"
-                  alt=""
-                  className="object-cover w-20 h-20 rounded-full dark:bg-gray-500"
-                />
-                <blockquote className="max-w-lg text-lg italic font-medium text-center">
-                  {" "}
-                  Et, dignissimos obcaecati. Recusandae praesentium doloribus
-                  vitae? Rem unde atque mollitia!
-                </blockquote>
-                <div className="text-center dark:text-gray-400">
-                  <p>Leroy Jenkins</p>
-                  <p>CEO of Company Co.</p>
-                </div>
-                <div className="flex space-x-2">
-                  <button
-                    type="button"
-                    aria-label="Page 1"
-                    className="w-2 h-2 rounded-full dark:bg-gray-50"
-                  ></button>
-                  <button
-                    type="button"
-                    aria-label="Page 2"
-                    className="w-2 h-2 rounded-full dark:bg-gray-600"
-                  ></button>
-                  <button
-                    type="button"
-                    aria-label="Page 3"
-                    className="w-2 h-2 rounded-full dark:bg-gray-600"
-                  ></button>
-                  <button
-                    type="button"
-                    aria-label="Page 4"
-                    className="w-2 h-2 rounded-full dark:bg-gray-600"
-                  ></button>
-                </div>
-              </div>
+
               <div className="p-8 space-y-8 rounded-md lg:col-span-full lg:py-12 border-gray-900 dark:border-gray-300  bg-gray-400 dark:bg-gray-900">
                 <h2 className="text-5xl font-bold dark:text-gray-50">
                   Create with us
@@ -164,28 +118,6 @@ const Section = () => {
             </section>
             <section>
               <div className="grid gap-6 lg:grid-cols-3">
-                <div className="overflow-hidden rounded border-gray-900 dark:border-gray-300 lg:flex lg:col-span-3">
-                  <Image
-                    width={1000}
-                    height={1000}
-                    src="https://source.unsplash.com/random/485x365"
-                    alt=""
-                    className="object-cover w-full h-auto max-h-96 bg-gray-400 dark:bg-gray-500"
-                  />
-                  <div className="p-6 space-y-6 lg:p-8 md:flex md:flex-col bg-gray-400 dark:bg-gray-900">
-                    <span className="self-start px-3 py-1 text-sm rounded-full dark:bg-violet-400 dark:text-gray-900">
-                      Business
-                    </span>
-                    <h2 className="text-xl md:flex-1">
-                      {`Already have a project? We're here to improve it. Looking for talented developers? Our pool of skilled professionals is ready
-                                         to showcase their expertise through part-time, full-time, or contract opportunities.`}
-                    </h2>
-                    <div>
-                      <p className="dark:text-gray-400">November 30, 2020</p>
-                      <p className="dark:text-gray-400">By Leroy Jenkins</p>
-                    </div>
-                  </div>
-                </div>
                 <div className="flex flex-col gap-3 justify-center align-center p-6 bg-gray-400 border border-gray-900 dark:border-gray-300   rounded lg:p-8 lg:py-12 dark:bg-gray-900">
                   <h3 className="font-medium dark:text-gray-50 text-lg">
                     Engaging
