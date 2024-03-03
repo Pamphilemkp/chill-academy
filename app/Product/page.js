554f7b4b313500b8aa4dbcfc492b274cbe4b5ff4
basitali111 <allahbadshah148@gmail.com>
2024-03-03 11:44:45 +0500

    Add product section

---
+++ app/Product/page.js
@@ -0,0 +1,15 @@
+import ProductComponent from "@/components/ProductComponent";
+
+export const metadata = {
+  title: 'Faqs',
+  description: 'Explore our comprehensive FAQ section to find answers to commonly asked questions about Chill Academy, providing valuable insights and information to enhance your understanding of our offerings and services...',
+};
+
+export default function Home() {
+
+    return (
+      <div >
+      <ProductComponent />
+    </div>
+    )
+  }
